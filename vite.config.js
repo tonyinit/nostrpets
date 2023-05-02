5fee3fb34c87af30afc99d02fa40e05e31aa2032
tonyinit <122396119+tonyinit@users.noreply.github.com>
2023-05-02 16:42:52 -0400

    add baseurl to work inside nostrpets subdirectory

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nostrpets/', // hitony.com/nostrpets
   plugins: [
     vue({
       template: { transformAssetUrls }
